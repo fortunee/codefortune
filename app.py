1e781673ba8ab1109b06beb42f1d18ebd55d2c65
fortune <fortune_ik@yahoo.com>
2016-05-28 22:57:14 +0100

    refactoring by mapping urls to functions

--- app.py
+++ app.py
@@ -7,13 +7,32 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/learn_more')
 def LearnMore():
     return render_template('learn_more_clicked.html')
 
+
 @app.route('/projects')
 def Projects():
     return render_template('/public_access/projects.html')
+
+
+@app.route('/blogs')
+def Blogs():
+    return 'Blogs undergoing maintainance. Please check back later'
+
+
+@app.route('/about')
+def About():
+    return render_template('/public_access/about.html')
+
+
+@app.route('/contact', methods=)
+def Contact():
+    return render_template('/public_access/contact.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
